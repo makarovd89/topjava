5e9e6802199cf9c8a4ca352c69e39268d37d18a3
makarovd89 <makarovd89@yahoo.com>
2018-03-09 19:59:00 +0600

    2 17 Add authorization

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -3,12 +3,17 @@ package ru.javawebinar.topjava;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class AuthorizedUser {
+    private static int authorizedId = 1;
 
     public static int id() {
-        return 1;
+        return authorizedId;
     }
 
     public static int getCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
+    }
+
+    public static void setId(int id){
+        authorizedId = id;
     }
 }
