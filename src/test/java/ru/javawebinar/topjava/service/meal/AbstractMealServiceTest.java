8f1306ca8209f02453444c4ceebc5a9662c03259
makarovd89 <makarovd89@yahoo.com>
2018-04-04 04:20:44 +0600

    5 11 Add tests for profiles

---
+++ src/test/java/ru/javawebinar/topjava/service/meal/AbstractMealServiceTest.java
@@ -0,0 +1,73 @@
+package ru.javawebinar.topjava.service.meal;
+
+import org.junit.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.AbstractServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+
+public abstract class AbstractMealServiceTest extends AbstractServiceTest {
+    @Test
+    public void testDelete() throws Exception {
+        mealService.delete(MEAL1_ID, USER_ID);
+        assertMatch(mealService.getAll(USER_ID), MEAL6, MEAL5, MEAL4, MEAL3, MEAL2);
+    }
+
+    @Test
+    public void testDeleteNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        mealService.delete(MEAL1_ID, 1);
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        Meal created = getCreated();
+        mealService.create(created, USER_ID);
+        assertMatch(mealService.getAll(USER_ID), created, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        Meal actual = mealService.get(ADMIN_MEAL_ID, ADMIN_ID);
+        assertMatch(actual, ADMIN_MEAL1);
+    }
+
+    @Test
+    public void testGetNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        mealService.get(MEAL1_ID, ADMIN_ID);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Meal updated = getUpdated();
+        mealService.update(updated, USER_ID);
+        assertMatch(mealService.get(MEAL1_ID, USER_ID), updated);
+    }
+
+    @Test
+    public void testUpdateNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Not found entity with id=" + MEAL1_ID);
+        mealService.update(MEAL1, ADMIN_ID);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        assertMatch(mealService.getAll(USER_ID), MEALS);
+    }
+
+    @Test
+    public void testGetBetween() throws Exception {
+        assertMatch(mealService.getBetweenDates(
+                LocalDate.of(2015, Month.MAY, 30),
+                LocalDate.of(2015, Month.MAY, 30), USER_ID), MEAL3, MEAL2, MEAL1);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/user/AbstractUserServiceTest.java
@@ -0,0 +1,73 @@
+package ru.javawebinar.topjava.service.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.dao.DataAccessException;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.AbstractServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collections;
+import java.util.List;
+
+import static ru.javawebinar.topjava.UserTestData.*;
+
+public abstract class AbstractUserServiceTest extends AbstractServiceTest {
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Before
+    public void setUp() throws Exception {
+        cacheManager.getCache("users").clear();
+    }
+
+    @Test
+    public void create() throws Exception {
+        User newUser = new User(null, "New", "new@gmail.com", "newPass", 1555, false, Collections.singleton(Role.ROLE_USER));
+        User created = userService.create(newUser);
+        newUser.setId(created.getId());
+        assertMatch(userService.getAll(), ADMIN, newUser, USER);
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void duplicateMailCreate() throws Exception {
+        userService.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.ROLE_USER));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        userService.delete(USER_ID);
+        assertMatch(userService.getAll(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void notFoundDelete() throws Exception {
+        userService.delete(1);
+    }
+
+    @Test
+    public void get() throws Exception {
+        User user = userService.get(USER_ID);
+        assertMatch(user, USER);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() throws Exception {
+        userService.get(1);
+    }
+
+    @Test
+    public void getByEmail() throws Exception {
+        User user = userService.getByEmail("user@yandex.ru");
+        assertMatch(user, USER);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<User> all = userService.getAll();
+        assertMatch(all, ADMIN, USER);
+    }
+}
